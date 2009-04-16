d224aefdc6fccbc5c04c1bd5c565f95747c37bbf
Alexander Ivanov <alexander.ivanov@redsolution.ru>
2009-04-16 12:16:41 +0600

    Remove revstore dir from dbschema dir

--- rs/dbschema/models.py
+++ rs/dbschema/models.py
@@ -3,7 +3,7 @@
 import os
 import re
 import sys
-from datetime import datetime
+from datetime import datetime, timedelta
 from os.path import join, dirname, abspath, exists, split
 
 from django.core.management.sql import sql_all, sql_delete
@@ -12,9 +12,12 @@ from django.conf import settings
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
-REVSTORE = 'revstore'
-REVSTORE_DIR = join(dirname(abspath(__file__)), REVSTORE)
-
+TEMPLATE_DIR = dirname(abspath(__file__))
+if hasattr(settings, 'REVSTORE_DIR'):
+    REVSTORE_DIR = settings.REVSTORE_DIR
+else:
+    REVSTORE_DIR = join(dirname(dirname(dirname(abspath(__file__)))), 'revstore')
+    
 class Repository(models.Model):
     '''
         Class representing database schema repository
@@ -25,6 +28,12 @@ class Repository(models.Model):
 
     revno = models.IntegerField(verbose_name=_('revision number'), default=0)
     applied = models.DateTimeField(verbose_name=_('applied'), auto_now_add=True)
+
+    @classmethod
+    def make_dir(cls):
+        if not exists(REVSTORE_DIR):
+            os.makedirs(REVSTORE_DIR)
+            open(join(REVSTORE_DIR, '__init__.py'), 'w')
     
     @classmethod
     def set_revno(cls, revno):
@@ -59,38 +68,54 @@ class Repository(models.Model):
         '''
             Gets revision list
         '''
+        insert = REVSTORE_DIR not in sys.path
+        if insert:
+            sys.path.insert(0, REVSTORE_DIR)
         try:
-            module = __import__(REVSTORE, globals(), locals(), [''])
-            return module.revisions
+            module = __import__('sequence', globals(), locals(), [''])
+            result = module.revisions
         except ImportError:
-            return []
+            result = []
+        if insert:
+            sys.path.remove(REVSTORE_DIR)
+        return result
 
     @classmethod
     def set_revisions(cls, revisions):
         '''
             Update revision list
         '''
-        template_file = open(join(REVSTORE_DIR, '__init__.template'), 'r')
+        template_file = open(join(TEMPLATE_DIR, 'sequence.template'), 'r')
         template = template_file.read()
-
-        init_file = open(join(REVSTORE_DIR, '__init__.py'), 'w')
+        cls.make_dir()
+        init_file = open(join(REVSTORE_DIR, 'sequence.py'), 'w')
         init_file.write(template % (
-            '[\n%s]' % ',\n'.join([repr(revision) 
+            '[\n%s]' % ',\n'.join([repr(revision)
                 for revision in revisions] + [''])))
-        
+
     @classmethod
     def count(cls):
         return len(cls.get_revisions())
 
     @classmethod
     def execute(cls, revision, upgrade=True):
-        module = __import__('%s.%s' % (REVSTORE, revision), globals(), locals(), 
-            ['upgrade', 'downgrade'])
+        insert = REVSTORE_DIR not in sys.path
+        if insert:
+            sys.path.insert(0, REVSTORE_DIR)
+        try:
+            module = __import__(revision, globals(), locals(), ['upgrade', 'downgrade'])
+        except ImportError:
+            module = None
+        if insert:
+            sys.path.remove(REVSTORE_DIR)
+        if module is None:
+            print 'Can`t find revision'
+            return False
         if upgrade:
             return module.upgrade()
         else:
             return module.downgrade()
-    
+
     @classmethod
     def apply(cls, revno):
         '''
@@ -124,51 +149,48 @@ class Repository(models.Model):
         '''
             Adds template for next revision and adds it into __init__.py
         '''
-        template_file = open(join(REVSTORE_DIR, 'revision.template'), 'r')
+        template_file = open(join(TEMPLATE_DIR, 'revision.template'), 'r')
         template = template_file.read().encode('utf-8')
 
         userid = 'unknown'
         for var in ['USER', 'USERNAME']:
             userid = os.environ.get(var, userid)
         userid = re.sub(r'[^A-Za-z0-9]', '_', userid)
-        iteration = 0
+        time = datetime.now()
         while True:
-            if iteration:
-                postfix = '_%d' % (iteration + 1)
-            else:
-                postfix = '' 
-            revision = 'revision_%s_%s%s' % (
-                datetime.now().strftime('%Y_%m_%d__%H_%M'), userid, postfix)
-            
+            revision = 'revision_%s_%s' % (
+                time.strftime('%Y_%m_%d__%H_%M'), userid)
+
+            cls.make_dir()
             revision_name = join(REVSTORE_DIR, '%s.py' % revision)
             if not exists(revision_name):
                 revision_file = open(revision_name, 'w')
                 revision_file.write(template % (upgrade_sql, downgrade_sql))
                 break
-            iteration = iteration + 1
-        
+            time = time + timedelta(minutes=1)
+
         revisions = cls.get_revisions()
         revisions.append(revision)
         cls.set_revisions(revisions)
-    
+
         print 'New revision was added, change it:\n%s' % revision_name
-        
+
     @classmethod
     def init(cls, application_names):
-        applications = [models.get_app(application_name) 
-            for application_name in application_names]  
+        applications = [models.get_app(application_name)
+            for application_name in application_names]
         upgrade = u''.join([
             u'\n'.join(sql_all(application, no_style()) + ['']).encode('utf-8')
                 for application in applications])
         downgrade = u''.join([u'\n'.join(sql_delete(application, no_style()) + ['']).encode('utf-8')
                 for application in applications])
         cls.add(upgrade, downgrade)
-        
+
     @classmethod
     def initall(cls):
         cls.init([application.__name__.split('.')[-2] 
             for application in models.get_apps()])
-    
+
     @classmethod
     def reset(cls):
         '''
